c66ea718bf8090503695688c5e517185f74558d0
FabianBartl <fabian@informatic-freak.de>
2022-12-07 15:56:47 +0100

    add example and my input for day 7

---
+++ 2022/07/main.py
@@ -0,0 +1,23 @@
+
+# My program structure template for AoC:
+
+import sys
+
+
+def main():
+	# read data file
+	inputFile = sys.argv[1] if len(sys.argv) >= 2 else "example.txt"
+	inputData = []
+	with open(inputFile, "r") as fobj:
+		lines = fobj.readlines()
+	
+	# solve it
+	for line in lines:
+		line = line.strip()
+	
+	# result
+	print("Part 1:")
+
+
+if __name__ == "__main__":
+	main()
