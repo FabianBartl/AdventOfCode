a808c516ffbda1129da9741257580ed8ec7c8b28
FabianBartl <fabian@informatic-freak.de>
2022-12-08 21:01:39 +0100

    add solution for day 8

--- 2022/08/main.py
+++ 2022/08/main.py
@@ -6,18 +6,16 @@ class Tree:
 	def __init__(self, height, visible=False):
 		self.height = height
 		self.visible = visible
+		self.score = 0
 	
 	def __repr__(self):
-		return f"Tree({len(self)}, {bool(self)})"
+		return f"Tree({len(self)}, {'#' if self else '_'}, {self.score})"
 	
 	def __len__(self):
 		return int(self.height)
 	
 	def __bool__(self):
 		return bool(self.visible)
-	
-	def __lt__(self, otherTree):
-		return len(self) < len(otherTree)
 	
 	def __gt__(self, otherTree):
 		return len(self) > len(otherTree)
@@ -28,28 +26,70 @@ class Forest:
 		self.rowsNum = len(self.treeGrid)
 		self.columnsNum = len(self.treeGrid[0])
 	
-	def __repr__(self):
+	def countVisibleTrees(self):
+		visibleTrees = 0
+		for row in self:
+			for tree in row:
+				if tree:
+					visibleTrees += 1
+		return visibleTrees
+	
+	def __repr__(self, *, onlyVisibility=False):
 		gridStr = ""
 		for num, row in enumerate(self.treeGrid):
 			if num == 0:
 				gridStr += " ["
 			else:
 				gridStr += "  "
-			gridStr += "[" + "".join([ "#" if tree else "_" for tree in row ]) + "]"
+			if onlyVisibility:
+				treeStr = "".join([ f"{'#' if tree else '_'}" for tree in row ])
+			else:
+				treeStr = "".join([ f" {len(tree)} {'#' if tree else '_'} {tree.score} " for tree in row ])
+			gridStr += "[" + treeStr + "]"
 			if num != self.rowsNum-1:
 				gridStr += ",\n"
 			else:
 				gridStr += "]"
 		return f"Forest(\n{gridStr}\n)"
 	
-	def __len__(self):
-		return self.rowsNum * self.columnsNum
+	def __iter__(self):
+		return iter(self.treeGrid)
+
+
+def setVisibility(forest):
+	# look from left
+	for rowNum, row in enumerate(forest):
+		maxHeight = len(forest.treeGrid[rowNum][0])
+		for colNum, tree in enumerate(row):
+			if (currentHeight := len(tree)) > maxHeight:
+				tree.visible = True
+				maxHeight = currentHeight
 	
-	def __getitem__(self, row, column):
-		return self.treeGrid[row][column]
+	# look from right
+	for rowNum, row in enumerate(forest):
+		maxHeight = len(forest.treeGrid[rowNum][-1])
+		for colNum, tree in enumerate(row[::-1]):
+			if (currentHeight := len(tree)) > maxHeight:
+				tree.visible = True
+				maxHeight = currentHeight
 	
-	def __setitem__(self, row, column, treeVisibility):
-		self.treeGrid[row][column].visible = bool(treeVisibility)
+	# look from top
+	for colNum in range(forest.columnsNum):
+		maxHeight = len(forest.treeGrid[0][colNum])
+		for rowNum in range(forest.rowsNum):
+			tree = forest.treeGrid[rowNum][colNum]
+			if (currentHeight := len(tree)) > maxHeight:
+				tree.visible = True
+				maxHeight = currentHeight
+	
+	# look from top
+	for colNum in range(forest.columnsNum):
+		maxHeight = len(forest.treeGrid[-1][colNum])
+		for rowNum in range(1, forest.rowsNum+1):
+			tree = forest.treeGrid[-rowNum][colNum]
+			if (currentHeight := len(tree)) > maxHeight:
+				tree.visible = True
+				maxHeight = currentHeight
 
 
 def main():
@@ -61,21 +101,57 @@ def main():
 		treeGrid = []
 		for num, line in enumerate(lines):
 			line = line.strip()
-			
 			treeRow = []
 			for pos, char in enumerate(line):
 				visibility = (pos == 0 or pos == len(line)-1) or (num == 0 or num == len(lines)-1)
 				treeRow.append(Tree(int(char), visibility))
-			
 			treeGrid.append(treeRow)
-		
 		forest = Forest(treeGrid)
 	
-	# look from top
-	print(forest)
+	# Part 1: get total number of visible trees
+	setVisibility(forest)
+	totalVisibleTrees = forest.countVisibleTrees()
+	
+	# Part 2: get tree score with the best viewing distance in all directions
+	scoreList = []
+	for r in range(forest.rowsNum):
+		for c in range(forest.columnsNum):
+			tree = forest.treeGrid[r][c]
+			
+			# from left to right
+			counterRight = 0
+			for colNum in range(c+1, forest.columnsNum):
+				counterRight += 1
+				if len(forest.treeGrid[r][colNum]) >= len(tree):
+					break
+			
+			# from right to left
+			counterLeft = 0
+			for colNum in range(-(forest.columnsNum-c+1), -(forest.columnsNum+1), -1):
+				counterLeft += 1
+				if len(forest.treeGrid[r][colNum]) >= len(tree):
+					break
+			
+			# from top to bottom
+			counterBottom = 0
+			for rowNum in range(r+1, forest.rowsNum):
+				counterBottom += 1
+				if len(forest.treeGrid[rowNum][c]) >= len(tree):
+					break
+			
+			# from bottom to top
+			counterTop = 0
+			for rowNum in range(-(forest.rowsNum-r+1), -(forest.rowsNum+1), -1):
+				counterTop += 1
+				if len(forest.treeGrid[rowNum][c]) >= len(tree):
+					break
+			
+			scoreList.append(counterRight * counterLeft * counterBottom * counterTop)
 	
 	# result
-	print("Part 1:")
+	print(forest.__repr__(onlyVisibility=True))
+	print("Part 1:", totalVisibleTrees)
+	print("Part 2:", max(scoreList))
 
 
 if __name__ == "__main__":
