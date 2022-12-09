b5bb8d36da23be97571781c377cce2b477ca5b56
FabianBartl <fabian@informatic-freak.de>
2022-12-09 15:12:34 +0100

    solve part 1 of day 9

--- 2022/09/main.py
+++ 2022/09/main.py
@@ -6,72 +6,124 @@ class Grid:
 	def __init__(self, cellGrid=dict()):
 		self.cellGrid = cellGrid
 	
-	def render(self):
-		raise NotImplementedError
+	def render(self, start, rope):
+		minX, maxX = self.max(start)
+		minY, maxY = self.min(start)
+		for y in range(maxY-1, minY+1, -1):
+			for x in range(minX-1, maxX+1):
+				if (x,y) == rope.head:
+					cell = "H"
+				elif (x,y) == rope.tail:
+					cell = "T"
+				elif (x,y) == start:
+					cell = "s"
+				else:
+					cell = "#" if self.cellGrid.get((x,y)) else "."
+				print(cell, end="")
+			print("\n", end="")
 	
 	def __repr__(self):
-		return self.cellGrid.__repr__()
+		return f"Grid(cellGrid={self.cellGrid.__repr__()})"
+	
+	def min(self, start=(0,0)):
+		minX, minY = start
+		for x, y in self.cellGrid:
+			if x < minX and y < minY:
+				minX, minY = x, y
+		return minX, minY
+	
+	def max(self, start=(0,0)):
+		maxX, maxY = start
+		for x, y in self.cellGrid:
+			if x > maxX and y > maxY:
+				maxX, maxY = x, y
+		return maxX, maxY
 	
 	def __getitem__(self, xy):
 		(xy := list(xy)).append(None)
 		return self.cellGrid.get((xy[0], xy[1]), xy[2])
-
-class Cell:
-	def __init__(self, visited=False):
-		self.visited = visited
 	
-	def render(self):
-		return "#" if self.visited else "_"
+	def __setitem__(self, xy, value):
+		self.cellGrid[xy] = value
 	
-	def __repr__(self):
-		return f"Cell(visited={self.visited})"
-	
-	def __bool__(self):
-		return self.visited
+	def __len__(self):
+		return len(self.cellGrid)
 
 class Rope:
-	def __init__(self, x, y, *, head=None, tail=None):
-		self.x = x
-		self.y = y
+	def __init__(self, head, tail):
 		self.head = head
 		self.tail = tail
 	
 	def __repr__(self):
-		return f"Rope(x={self.x}, y={self.y}, head={self.head}, tail={self.tail})"
-
-	def isFirst(self):
-		return self.head is None
-
-	def isLast(self):
-		return self.tail is None
+		return f"Rope(head={self.head}, tail={self.tail})"
 	
+	def move(self, direction, grid):
+		self.head = list(self.head)
+		if direction == "R":
+			self.head[0] += 1
+		elif direction == "U":
+			self.head[1] += 1
+		elif direction == "L":
+			self.head[0] -= 1	
+		elif direction == "D":
+			self.head[1] -= 1
+		self.head = tuple(self.head)
+	
+	def update(self, grid):
+		xDiff, yDiff = self.head[0]-self.tail[0], self.head[1]-self.tail[1]
+		self.tail = list(self.tail)
+		if xDiff > 1:
+			self.tail[0] += 1
+		elif xDiff < -1:
+			self.tail[0] -= 1
+		elif yDiff > 1:
+			self.tail[1] += 1
+		elif yDiff < -1:
+			self.tail[1] -= 1
+		
+		if yDiff > 1 and (xDiff == 1 or xDiff == -1):
+			self.tail[0] += xDiff
+		elif yDiff < -1 and (xDiff == 1 or xDiff == -1):
+			self.tail[0] += xDiff
+		elif xDiff > 1 and (yDiff == 1 or yDiff == -1):
+			self.tail[1] += yDiff
+		elif xDiff < -1 and (yDiff == 1 or yDiff == -1):
+			self.tail[1] += yDiff
+		self.tail = tuple(self.tail)
+
 	def __len__(self):
-		raise NotImplementedError
+		return max(self.head[0]-self.tail[0], self.head[1]-self.tail[1])
 
 
 def main():
-	grid = Grid()
-
-	head = Rope(0,0)
-	tail = Rope(0,0, head=head)
-	head.tail = tail
-
 	# read data file
 	inputFile = sys.argv[1] if len(sys.argv) >= 2 else "example.txt"
-	moves = []
+	movements = []
 	with open(inputFile, "r") as fobj:
 		lines = fobj.readlines()
 		
 		for line in lines:
 			line = line.strip().split(" ")
-			moves.extend([ line[0] for _ in range(int(line[1])) ])
+			movements.extend([ line[0] for _ in range(int(line[1])) ])
+	
+	start = (0,0)
+	grid = Grid({start: True})
+	rope = Rope(start, start)
 	
 	# do movement
-	for move in moves:
-		pass
+	for direction in movements:
+		rope.move(direction, grid)
+		rope.update(grid)
+		grid[rope.tail] = True
+
+		# grid.render(start, rope)
+		# print(direction, rope, grid, sep="\n")
+		# input()
 	
-	# result
-	print("Part 1:")
+	grid.render(start, rope)
+	
+	# results
+	print("Part 1:", len(grid))
 
 
 if __name__ == "__main__":
