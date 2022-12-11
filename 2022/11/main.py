931daa0802af4ecde7d3baf948b8b5fd5215c116
FabianBartl <fabian@informatic-freak.de>
2022-12-11 13:02:16 +0100

    add copy method / relieved option for part 1 and 2, display progress with tqdm

--- 2022/11/main.py
+++ 2022/11/main.py
@@ -1,5 +1,6 @@
 
 import sys, re
+from tqdm import tqdm
 
 
 class Monkey:
@@ -11,19 +12,23 @@ class Monkey:
 		self.decision  : dict[bool:Monkey] = decision
 		self.inspections = 0
 	
-	def inspect(self):
+	def inspect(self, relieved):
 		self.inspections += 1
 		item = self.items.pop(0)
 		item = self.operation(item)
-		item //= 3
+		if relieved:
+			item //= 3
 		self.decision[ item % self.test == 0 ].catch(item)
 	
-	def inspectAll(self):
+	def inspectAll(self, relieved):
 		for _ in range(len(self.items)):
-			self.inspect()
+			self.inspect(relieved)
 	
 	def catch(self, item):
 		self.items.append(item)
+	
+	def copy(self):
+		return Monkey(self.name, self.items.copy(), self.operation, self.test, self.decision.copy())
 	
 	def __repr__(self):
 		return f"Monkey(name='{self.name}', items={self.items}, operation=<lambda function>, test={self.test}, decision={'{'}True: Monkey(name='{self.decision[True].name}'), False: Monkey(name='{self.decision[False].name}'){'}'}, inspections={self.inspections})"
@@ -41,9 +46,33 @@ def parseMonkey(lines):
 	decision = {True: ifTrue, False: ifFalse}
 	
 	return Monkey(name, items, operation, test, decision)
-	
 
-def main():
+
+def main(monkeys, rounds, relieved, part):
+	# replace monkey names by Monkey objects in decision dictionary
+	for monkey in monkeys:
+		monkey.decision[True ] = monkeys[ int(monkey.decision[True ]) ]
+		monkey.decision[False] = monkeys[ int(monkey.decision[False]) ]
+
+	# do all rounds
+	for roundNum in tqdm(range(rounds), desc="Rounds"):
+		# do round
+		for monkey in monkeys:
+			# do turns
+			while len(monkey.items) > 0:
+				monkey.inspectAll(relieved)
+	
+	print(f"\nAfter Round: {rounds}")
+	print("\n" + "\n\n".join([ monkey.__repr__() for monkey in monkeys ]))
+
+	monkeys.sort( key=lambda x: x.inspections, reverse=True )
+	monkeyBusiness = monkeys[0].inspections * monkeys[1].inspections
+	
+	# result
+	print(f"\nPart {part}: {monkeyBusiness}\n\n")
+
+
+if __name__ == "__main__":
 	# read data file
 	inputFile = sys.argv[1] if len(sys.argv) >= 2 else "example.txt"
 	with open(inputFile, "r") as fobj:
@@ -53,35 +82,6 @@ def main():
 		monkeys = []
 		for lineNum in range(monkeyCount):
 			monkeys.append( parseMonkey(lines[lineNum*7:(lineNum+1)*7]) )
-	
-	# replace monkey names by Monkey objects in decision dictionary
-	for monkey in monkeys:
-		monkey.decision[True ] = monkeys[ int(monkey.decision[True ]) ]
-		monkey.decision[False] = monkeys[ int(monkey.decision[False]) ]
 
-	print(monkeys, "\n")
-	
-	# repeat it
-	rounds = 20
-	for roundNum in range(rounds):
-		print("\n\nAfter Round:", roundNum)
-		print("\n" + "\n\n".join([ monkey.__repr__() for monkey in monkeys ]))
-		input()
-		# do round
-		for monkey in monkeys:
-			# do turns
-			while len(monkey.items) > 0:
-				monkey.inspectAll()
-	
-	print("\n\nAfter Round:", rounds)
-	print("\n" + "\n\n".join([ monkey.__repr__() for monkey in monkeys ]))
-
-	monkeys.sort( key=lambda x: x.inspections, reverse=True )
-	monkeyBusiness = monkeys[0].inspections * monkeys[1].inspections
-	
-	# result
-	print("\nPart 1:", monkeyBusiness)
-
-
-if __name__ == "__main__":
-	main()
+	main([ monkey.copy() for monkey in monkeys ],    20, True,  1)
+	main([ monkey.copy() for monkey in monkeys ], 1_000, False, 2)
